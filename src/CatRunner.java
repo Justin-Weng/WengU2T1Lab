204b9cb1b2d93859779fe089b72ecd809b0e77e9
BT_1n3_22 <sample@bths.edu>
2023-09-22 11:03:22 -0400

    Added cats

---
+++ src/CatRunner.java
@@ -0,0 +1,7 @@
+public class CatRunner {
+    public static void main(String[] arg) {
+        Cat cat1 = new Cat("cat1", 999, 1.1);
+        cat1.introduce();
+        cat1.printCatInfo();
+    }
+}
